ad3ec4f4a21d764d88da5c9b47d5c90351014b8c
erika-h7 <erika.herrera1@outlook.com>
2022-10-31 11:13:10 -0700

    Implemented UIImagePickerControllerDelegate also incorporated a CoreML Model in our app

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreML
+import  Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // created a new instance of the object
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // set all the properties of that object
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickerImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickerImage
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
+        
+        
+    }
+    
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        // when camera icon is tapped we are asking the app to present this imagePicker to the user so they can use the camera or photo album
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
