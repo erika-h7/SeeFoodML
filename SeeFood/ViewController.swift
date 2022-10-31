d6f9ee7008982d2c5bcee31c0bc1199d03533ce5
erika-h7 <erika.herrera1@outlook.com>
2022-10-31 12:27:39 -0700

    Fetched data from the CoreML Model and compared with our desired result.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -10,7 +10,7 @@ import CoreML
 import  Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     // created a new instance of the object
@@ -32,6 +32,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickerImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickerImage
+            
+            guard let ciimage = CIImage(image: userPickerImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true)
@@ -40,7 +46,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
-
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog! 🌭"
+                } else {
+                    self.navigationItem.title = "🚫 Not Hotdog!"
+                }
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+        
+    }
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         // when camera icon is tapped we are asking the app to present this imagePicker to the user so they can use the camera or photo album
